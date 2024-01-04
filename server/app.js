9d9609239264940cfdf0b672396d5956a2bc73c7
Cory Taylor <official.cstdesign@gmail.com>
2024-01-03 20:53:29 -0600

    9.2 deploy to Render.com

--- server/app.js
+++ server/app.js
@@ -1,11 +1,55 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import pizzas from "./routers/pizzas.js";
 
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB, {
+  // Configuration options to remove deprecation warnings, just include them to remove clutter
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
 const PORT = process.env.PORT || 4040;
+
+// Logging Middleware
+const logging = (request, response, next) => {
+  console.log(
+    `${request.method} ${request.url} ${new Date().toLocaleString("en-us")}`
+  );
+  next();
+};
+app.use(logging);
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+app.use(cors);
+app.use(express.json());
+app.use("/pizzas", pizzas);
 
 // Request handlers go here
 app.get("/status", (request, response) => {
@@ -51,31 +95,6 @@ app.get("/weather/:city", (request, response) => {
     city
   });
 });
-
-// Middleware
-const logging = (request, response, next) => {
-  console.log(
-    `${request.method} ${request.url} ${new Date().toLocaleString("en-us")}`
-  );
-  next();
-};
-app.use(logging);
-
-// CORS Middleware
-const cors = (req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type, Accept,Authorization,Origin"
-  );
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-};
-app.use(cors);
 
 app.listen(PORT, () => console.log("Listening on port 4040"));
 
